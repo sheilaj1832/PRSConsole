674b4182969402edaad2ba87210d03144ddd6121
sheilaj1832 <sheilajohnson1832@gmail.com>
2018-09-18 21:45:21 -0400

    PRS Console business classes set up from mySQL

---
+++ src/business/PurchaseRequest.java
@@ -0,0 +1,122 @@
+package business;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class PurchaseRequest {
+
+	private int id;
+	private int userId;
+	private String description;
+	private String justification;
+	private LocalDate dateNeeded;
+	private String deliveryMode;
+	private String status;
+	private double total;
+	private LocalDateTime submittedDate;
+	private String reasonForRejection;
+
+	public PurchaseRequest(int id, int userId, String description, String justification, LocalDate dateNeeded,
+			String deliveryMode, String status, double total, LocalDateTime submittedDate, String reasonForRejection) {
+		super();
+		this.id = id;
+		this.userId = userId;
+		this.description = description;
+		this.justification = justification;
+		this.dateNeeded = dateNeeded;
+		this.deliveryMode = deliveryMode;
+		this.status = status;
+		this.total = total;
+		this.submittedDate = submittedDate;
+		this.reasonForRejection = reasonForRejection;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getJustification() {
+		return justification;
+	}
+
+	public void setJustification(String justification) {
+		this.justification = justification;
+	}
+
+	public LocalDate getDateNeeded() {
+		return dateNeeded;
+	}
+
+	public void setDateNeeded(LocalDate dateNeeded) {
+		this.dateNeeded = dateNeeded;
+	}
+
+	public String getDeliveryMode() {
+		return deliveryMode;
+	}
+
+	public void setDeliveryMode(String deliveryMode) {
+		this.deliveryMode = deliveryMode;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public double getTotal() {
+		return total;
+	}
+
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+	public LocalDateTime getSubmittedDate() {
+		return submittedDate;
+	}
+
+	public void setSubmittedDate(LocalDateTime submittedDate) {
+		this.submittedDate = submittedDate;
+	}
+
+	public String getReasonForRejection() {
+		return reasonForRejection;
+	}
+
+	public void setReasonForRejection(String reasonForRejection) {
+		this.reasonForRejection = reasonForRejection;
+	}
+
+	@Override
+	public String toString() {
+		return "PurchaseRequest [id=" + id + ", userId=" + userId + ", description=" + description + ", justification="
+				+ justification + ", dateNeeded=" + dateNeeded + ", deliveryMode=" + deliveryMode + ", status=" + status
+				+ ", total=" + total + ", submittedDate=" + submittedDate + ", reasonForRejection=" + reasonForRejection
+				+ "]";
+	}
+	
+}
---
+++ src/business/PurchaseRequestLineItem.java
@@ -0,0 +1,56 @@
+package business;
+
+public class PurchaseRequestLineItem {
+	
+	private int id;
+	private int purchaseRequestId;
+	private int productId;
+	private int quantity;
+
+	public PurchaseRequestLineItem(int id, int purchaseRequestId, int productId, int quantity) {
+		super();
+		this.id = id;
+		this.purchaseRequestId = purchaseRequestId;
+		this.productId = productId;
+		this.quantity = quantity;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getPurchaseRequestId() {
+		return purchaseRequestId;
+	}
+
+	public void setPurchaseRequestId(int purchaseRequestId) {
+		this.purchaseRequestId = purchaseRequestId;
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public String toString() {
+		return "PurchaseRequestLineItem [id=" + id + ", purchaseRequestId=" + purchaseRequestId + ", productId="
+				+ productId + ", quantity=" + quantity + "]";
+	}
+	
+}
